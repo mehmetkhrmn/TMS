3989ddfbfe8daaecbf8b0fcea7ef390698af598c
Mehmet Kahraman <kahramanmehmet065@gmail.com>
2026-08-06 17:53:44 +0300

    Validation
    Validation of models

--- internal/models/models.go
+++ internal/models/models.go
@@ -4,62 +4,64 @@ import "time"
 
 type Ticket struct {
 	ID            int       `json:"id"`
-	Subject       string    `json:"subject"`
-	Description   string    `json:"description"`
-	CustomerID    int       `json:"customer_id"`
-	CustomerEmail string    `json:"customer_email"`
+	Subject       string    `json:"subject" binding:"required"`
+	Description   string    `json:"description" binding:"required"`
+	CustomerID    int       `json:"customer_id" binding:"required"`
+	CustomerEmail string    `json:"customer_email" binding:"required,email"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 	Status        string    `json:"status"`
 }
-
 type Representative struct {
 	ID        int       `json:"id"`
-	Name      string    `json:"name"`
+	Name      string    `json:"name" binding:"required"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type Answer struct {
 	ID         int       `json:"id"`
-	AnswerText string    `json:"answer"`
-	RepID      int       `json:"representative_id"`
-	TicketID   int       `json:"ticket_id"`
+	AnswerText string    `json:"answer" binding:"required"`
+	RepID      int       `json:"representative_id" binding:"required"`
+	TicketID   int       `json:"ticket_id" binding:"required"`
 	AnsweredAt time.Time `json:"answered_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 }
 type Customer struct {
 	ID        int       `json:"id"`
-	Name      string    `json:"name"`
+	Name      string    `json:"name" binding:"required"`
 	CreatedAt time.Time `json:"created_at"`
-	Email     string    `json:"email"`
+	Email     string    `json:"email" binding:"required,email"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 type AuthUser struct {
 	ID           int       `json:"id"`
-	Username     string    `json:"username"`
-	PasswordHash string    `json:"passwordHash"`
-	Role         string    `json:"role"`
+	Username     string    `json:"username" binding:"required"`
+	PasswordHash string    `json:"passwordHash" binding:"required"`
+	Role         string    `json:"role" binding:"required"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
-	EntityId     int       `json:"entity_id"`
+	EntityId     int       `json:"entity_id" binding:"required"`
 }
 type LoginRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 type RegisterRequestRepresentative struct {
-	Name     string `json:"name"`
+	Name     string `json:"name" binding:"required"`
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
-	Role     string `json:"role" `
-	EntityID int    `json:"entity_id" `
 }
 type RegisterRequestCustomer struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
-	Role     string `json:"role" `
-	EntityID int    `json:"entity_id"`
+}
+type CreateTicketRequest struct {
+	Subject     string `json:"subject" binding:"required"`
+	Description string `json:"description" binding:"required"`
+}
+type CreateAnswerRequest struct {
+	AnswerText string `json:"answer" binding:"required"`
 }
